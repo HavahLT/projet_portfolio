5033b0df2ee1514981e9a3b7d8506d453a6cb8d1
HavahLT <tourbillon.laurianne@gmail.com>
2017-02-22 17:08:10 +0100

    update

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,14 @@
-var btnMenu = document.getElementById('btn-menu');
-var btnMenuClose = document.getElementById('btn-menu-close');
-var menuPrincipal = document.getElementById('menu-principal');
+// ici tous les scripts qui gèrent le menu de la version mobile :
+var btnMenu = document.getElementById('btn-menu'),
+    btnMenuClose = document.getElementById('btn-menu-close'),
+    btnProjets = document.getElementById('btn-projets'),
+    menuPrincipal = document.getElementById('menu-principal'),
+    sousMenuProjets = document.getElementById('sous-menu');
 
 btnMenuClose.style.display = "none";
 menuPrincipal.style.display = "none";
+sousMenuProjets.style.display = "none";
+btnProjets.style.cursor = "pointer";
 
 btnMenu.addEventListener('click', function(){
   menuPrincipal.style.display = "block";
@@ -15,4 +20,8 @@ btnMenuClose.addEventListener('click', function(){
   menuPrincipal.style.display = "none";
   btnMenu.style.display = "block";
   btnMenuClose.style.display = "none";
+});
+
+btnProjets.addEventListener('click', function(){
+  sousMenuProjets.style.display = "block";
 });
